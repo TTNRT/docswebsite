1434d6426761983233e375efaa61e6f1879d922f
Shaun Hoffer <75172901+SSP6904@users.noreply.github.com>
2026-03-29 14:56:34 -0400

    Reverted back to Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,109 +6,109 @@ import starlight from '@astrojs/starlight';
 const port = 8000
 const domain = import.meta.env.PROD ? 'https://docs.ttnrtsite.me' : `http://localhost:${port}`
 export default defineConfig({
-	site: domain,
-	output: 'static',
-	compressHTML: false,
-	trailingSlash: 'never',
-	server: {
-		host: true,
-		port: port
-	},
-	devToolbar: {
-		enabled: false
-	},
-	build: {
-		format: 'file'
-	},
-	image: {
-		service: {
-		   	entrypoint: 'astro/assets/services/sharp',
-		   	config: {
-			 	limitInputPixels: false,
-		  	},
-		},
-	},
-	integrations: [
-		starlight({
-			title: 'Corporate Documentation',
-			description: 'Our documentation website containing all of our knowledge base documents!',
-			social: [
-				{ icon: 'github', label: 'GitHub', href: 'https://github.com/TTNRT' }
-			],
-			logo: {
-				src: './src/assets/logo.png',
-			},
-			editLink: {
-				//baseUrl: 'https://pr.new/github.com/TTNRT/docswebsite/edit/main/'
-				baseUrl: 'https://github.dev/TTNRT/docswebsite/blob/main/'
-			},
-			pagination: false,
-			lastUpdated: true,
-			favicon: 'favicon.png',
-			customCss: [
-				'./src/styles/default-styles.css'
-			],
-			sidebar: [
-				{
-					label: 'Our policy documents',
-					items: [
-						{
-							label: 'Table of contents',
-							slug: 'polices'
-						},
-						{
-							label: 'Terms and conditions',
-							autogenerate: {directory: 'polices/terms-of-use'}
-						},
-						{
-							label: 'Contributions guidelines',
-							autogenerate: {directory: 'polices/contributions'}
-						},
-						{
-							label: 'Other documents',
-							autogenerate: {directory: 'polices/other'}
-						},
-					]
-				},
-				{
-					label: 'TTGit',
-					items: [
-						{ 
-							label: 'Workflow interfaces', 
-							autogenerate: {directory: 'ttgit/actions'}
-						},
-						{
-							label: 'Customize your account',
-							autogenerate: {directory: 'ttgit/customize'}
-						},
-						{
-							label: 'Authentication',
-							autogenerate: {directory: 'ttgit/authentication'}
-						},
-						{
-							label: "Using the API",
-							autogenerate: {directory: "ttgit/using-the-api"}
-						}
-					],
-				},
-				{
-					label: 'MyTT',
-					items: [
-						{
-							label: 'Account management',
-							autogenerate: {directory: 'mytt/account'}
-						},
-						{
-							label: 'Customize your profile',
-							autogenerate: {directory: 'mytt/customize'}
-						},
-						{
-							label: "Using the API",
-							autogenerate: {directory: "mytt/using-the-api"}
-						}
-					]
-				}
-			],
-		}),
-	],
+    site: domain,
+    output: 'static',
+    compressHTML: false,
+    trailingSlash: 'never',
+    server: {
+        host: true,
+        port: port
+    },
+    devToolbar: {
+        enabled: false
+    },
+    build: {
+        format: 'file'
+    },
+    image: {
+        service: {
+            entrypoint: 'astro/assets/services/sharp',
+            config: {
+                limitInputPixels: false,
+            },
+        },
+    },
+    integrations: [
+        starlight({
+            title: 'Corporate Documentation',
+            description: 'Our documentation website containing all of our knowledge base documents!',
+            social: [
+                { icon: 'github', label: 'GitHub', href: 'https://github.com/TTNRT' }
+            ],
+            logo: {
+                src: './src/assets/logo.png',
+            },
+            editLink: {
+                //baseUrl: 'https://pr.new/github.com/TTNRT/docswebsite/edit/main/'
+                baseUrl: 'https://github.dev/TTNRT/docswebsite/blob/main/'
+            },
+            pagination: false,
+            lastUpdated: true,
+            favicon: 'favicon.png',
+            customCss: [
+                './src/styles/default-styles.css'
+            ],
+            sidebar: [
+                {
+                    label: 'Our policy documents',
+                    items: [
+                        {
+                            label: 'Table of contents',
+                            slug: 'polices'
+                        },
+                        {
+                            label: 'Terms and conditions',
+                            autogenerate: { directory: 'polices/terms-of-use' }
+                        },
+                        {
+                            label: 'Contributions guidelines',
+                            autogenerate: { directory: 'polices/contributions' }
+                        },
+                        {
+                            label: 'Other documents',
+                            autogenerate: { directory: 'polices/other' }
+                        },
+                    ]
+                },
+                {
+                    label: 'TTGit',
+                    items: [
+                        {
+                            label: 'Workflow interfaces',
+                            autogenerate: { directory: 'ttgit/actions' }
+                        },
+                        {
+                            label: 'Customize your account',
+                            autogenerate: { directory: 'ttgit/customize' }
+                        },
+                        {
+                            label: 'Authentication',
+                            autogenerate: { directory: 'ttgit/authentication' }
+                        },
+                        {
+                            label: "Using the API",
+                            autogenerate: { directory: "ttgit/using-the-api" }
+                        }
+                    ],
+                },
+                {
+                    label: 'MyTT',
+                    items: [
+                        {
+                            label: 'Account management',
+                            autogenerate: { directory: 'mytt/account' }
+                        },
+                        {
+                            label: 'Customize your profile',
+                            autogenerate: { directory: 'mytt/customize' }
+                        },
+                        {
+                            label: "Using the API",
+                            autogenerate: { directory: "mytt/using-the-api" }
+                        }
+                    ]
+                }
+            ],
+        }),
+    ],
 });
